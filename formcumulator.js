66b32c65b24bd28b3af5a3f6dba335e71d0631cb
Alan Volker <volkeralan12@gmail.com>
2022-02-26 12:34:42 -0300

    En proceso de barra de progreso

--- formcumulator.js
+++ formcumulator.js
@@ -1,63 +1,48 @@
-var formas = 0;
+document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+var formas = 0.00;
 var acumuladores = 0;
 var inflacion = 1.0;
 var velocidad = 0;
+var precioBaseAcumuladores = 25.00;
+var precioAcumuladoresFinal = precioBaseAcumuladores * inflacion;
+var velocidadDeAnimacion = document.getElementsByClassName("progresoAcumulador").style.animation-duration
 
 var circulo = document.getElementById("circulo");
-
-function accionCirculo() {
-  circulo.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-titulo.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
-formas++;
-formasTexto.innerHTML = 'Formas ⬛: ' + formas;
-};
-
-
-var formasTexto = document.getElementById("formas")
-
-document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-
 var titulo = document.getElementById("titulo");
-
-
+var formasTexto = document.getElementById("formas")
 var botonAcumuladores = document.getElementById("botonAcumuladores");
-
 var cambiarColorDeFondo = document.getElementById("cambiarColorDeFondo");
-
-function cambiarColorDeFondo() {
-  document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-};
-
-
+var progresoAcumuladores = document.getElementById("progresoAcumuladores");
 var precioAcumuladores = document.getElementById("precioAcumuladores");
 
-//Generar progress tag debajo de los acumuladores.
+function accionCirculo() {
+    circulo.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    titulo.style.color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    formas++;
+    formasTexto.innerHTML = 'Formas ⚫: ' + formas.toFixed(2);
+};
 
-var progresoAcumuladores = document.getElementById("progresoAcumuladores");
-
+function colorFondo() {document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
 
 //Hacer que cuando se haga click al boton "Acumuladores" se resten las formas indicadas por el precio, y que la cantidad de acumuladores aumente en 1
-var precioBaseAcumuladores = 25;
-var precioAcumuladores = precioBaseAcumladores * inflacion;
+
 function compraAcumuladores() {
-    if (formas >= precioAcumuladores) {
-    formas -= precioAcumuladores;
+    if (formas >= precioAcumuladoresFinal) {
+    formas -= precioAcumuladoresFinal;
+    precioAcumuladoresFinal = precioBaseAcumuladores * inflacion;
     acumuladores += 1;
     velocidad +=1;
     botonAcumuladores.innerHTML = 'Acumuladores: ' + acumuladores;
-    precio.innerHTML = 'Precio: ' + precioAcumuladores + ' ⬛';
+    precioAcumuladores.innerHTML = 'Precio: ' + precioAcumuladoresFinal.toFixed(2) + ' ⚫';
+    formasTexto.innerHTML = "Formas ⚫: "+ formas.toFixed(2);
     inflacion *= 1.01
+
 }
 };
 //Hacer que el valor de progress aumente a una velocidad establecida por la variable del mismo nombre.
-var intervalo = setInterval(function() {
-progresoAcumuladores.value += velocidad;
-}, 1000);
+
+
 //Cuando se la progress bar este al 100%, se agregara la misma cantidad de formas que de acumuladores.
-function progresoAcumuladores() {
-if (progresoAcumuladores.value == 100) {
-formas += acumuladores;
-formasTexto.innerHTML = 'Formas ⬛: ' + formas;
-progresoAcumuladores.value = 0;
-}
-};
+
+
