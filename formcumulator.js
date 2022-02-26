27c219da17682a2ba76714ffd0b782c6b3b3f2a4
Alan Volker <volkeralan12@gmail.com>
2022-02-26 15:05:43 -0300

    Alpha 0.0.03 Jugable
    Falta agregar mas acumuladores y otras formas

--- formcumulator.js
+++ formcumulator.js
@@ -5,7 +5,7 @@ var inflacion = 1.0;
 var velocidad = 0;
 var precioBaseAcumuladores = 25.00;
 var precioAcumuladoresFinal = precioBaseAcumuladores * inflacion;
-var velocidadDeAnimacion = document.getElementsByClassName("progresoAcumulador").style.animation-duration
+// var velocidadDeAnimacion = document.getElementsByClassName("progresoAcumulador").style.anidurationmation-
 
 var circulo = document.getElementById("circulo");
 var titulo = document.getElementById("titulo");
@@ -27,22 +27,30 @@ function colorFondo() {document.body.style.backgroundColor = '#' + Math.floor(Ma
 
 //Hacer que cuando se haga click al boton "Acumuladores" se resten las formas indicadas por el precio, y que la cantidad de acumuladores aumente en 1
 
+function actualizarDatos(){
+    botonAcumuladores.innerHTML = 'Acumuladores: ' + acumuladores;
+    precioAcumuladores.innerHTML = 'Precio: ' + precioAcumuladoresFinal.toFixed(2) + ' ⚫';
+    formasTexto.innerHTML = "Formas ⚫: "+ formas.toFixed(2);
+}
+
 function compraAcumuladores() {
     if (formas >= precioAcumuladoresFinal) {
     formas -= precioAcumuladoresFinal;
     precioAcumuladoresFinal = precioBaseAcumuladores * inflacion;
     acumuladores += 1;
     velocidad +=1;
-    botonAcumuladores.innerHTML = 'Acumuladores: ' + acumuladores;
-    precioAcumuladores.innerHTML = 'Precio: ' + precioAcumuladoresFinal.toFixed(2) + ' ⚫';
-    formasTexto.innerHTML = "Formas ⚫: "+ formas.toFixed(2);
+    actualizarDatos();
     inflacion *= 1.01
 
 }
 };
+const animated = document.getElementById("contenedorAcumulador1");
+
+//Add animation event listener, with attached function.
+animated.addEventListener('animationiteration', () => {
+formas += acumuladores;
+actualizarDatos();
+});
 //Hacer que el valor de progress aumente a una velocidad establecida por la variable del mismo nombre.
-
-
-//Cuando se la progress bar este al 100%, se agregara la misma cantidad de formas que de acumuladores.
 
 
