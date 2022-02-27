6ad55ea3637638947c19a709d9d4d8abc1f91844
Alan Volker <volkeralan12@gmail.com>
2022-02-27 10:59:25 -0300

    actualizacion alpha.0.0.04
    Añadido el boton de spawner

--- formcumulator/formcumulator.js
+++ formcumulator/formcumulator.js
@@ -1,19 +1,24 @@
 document.body.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-var formas = 0.00;
-var acumuladores = 0;
-var inflacion = 1.0;
-var velocidad = 0;
-var precioBaseAcumuladores = 25.00;
-var precioAcumuladoresFinal = precioBaseAcumuladores * inflacion;
-// var velocidadDeAnimacion = document.getElementsByClassName("progresoAcumulador").style.anidurationmation-
+let formas = 0.00;
+let acumuladores = 0;
+let spawners = 0;
+let inflacion = 1.0;
+let velocidad = 0;
+let precioBaseAcumuladores = 25.00;
+let precioAcumuladoresFinal = precioBaseAcumuladores * inflacion;
+let precioBaseSpawners = 100.00;
+let precioSpawnersFinal = precioBaseSpawners * inflacion;
+// let //velocidadDeAnimacion = document.getElementsByClassName("progresoAcumulador").style.anidurationmation-
 
-var circulo = document.getElementById("circulo");
-var titulo = document.getElementById("titulo");
-var formasTexto = document.getElementById("formas")
-var botonAcumuladores = document.getElementById("botonAcumuladores");
-var cambiarColorDeFondo = document.getElementById("cambiarColorDeFondo");
-var progresoAcumuladores = document.getElementById("progresoAcumuladores");
-var precioAcumuladores = document.getElementById("precioAcumuladores");
+let circulo = document.getElementById("circulo");
+let titulo = document.getElementById("titulo");
+let formasTexto = document.getElementById("formas")
+let divAcumuladores = document.getElementById("progresoAcumulador");
+let divSpawners = document.getElementById("progresoSpawner");
+let cambiarColorDeFondo = document.getElementById("cambiarColorDeFondo");
+let progresoAcumuladores = document.getElementById("progresoAcumuladores");
+let precioAcumuladores = document.getElementById("precioAcumuladores");
+let precioSpawners = document.getElementById("precioSpawners");
 
 function accionCirculo() {
     circulo.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -28,9 +33,11 @@ function colorFondo() {document.body.style.backgroundColor = '#' + Math.floor(Ma
 //Hacer que cuando se haga click al boton "Acumuladores" se resten las formas indicadas por el precio, y que la cantidad de acumuladores aumente en 1
 
 function actualizarDatos(){
-    botonAcumuladores.innerHTML = 'Acumuladores: ' + acumuladores;
+    divAcumuladores.innerHTML = acumuladores;
     precioAcumuladores.innerHTML = 'Precio: ' + precioAcumuladoresFinal.toFixed(2) + ' ⚫';
     formasTexto.innerHTML = "Formas ⚫: "+ formas.toFixed(2);
+    precioSpawners.innerHTML = "Precio: " + precioSpawnersFinal.toFixed(2) + " ⚫";
+    divSpawners.innerHTML = spawners;
 }
 
 function compraAcumuladores() {
@@ -38,19 +45,34 @@ function compraAcumuladores() {
     formas -= precioAcumuladoresFinal;
     precioAcumuladoresFinal = precioBaseAcumuladores * inflacion;
     acumuladores += 1;
-    velocidad +=1;
+    //velocidad +=1;
     actualizarDatos();
     inflacion *= 1.01
 
 }
-};
-const animated = document.getElementById("contenedorAcumulador1");
 
+};
+function compraSpawners(){
+    if (formas >= precioSpawnersFinal) {
+        formas -= precioSpawnersFinal;
+        precioSpawnersFinal = precioBaseSpawners * inflacion;
+        spawners += 1;
+        //velocidad +=1;
+        actualizarDatos();
+        inflacion *= 1.01
+}
+}
 //Add animation event listener, with attached function.
+//Esto puede ser optimizado por una sola funcion, aunque se necesita mas laburo...
+const animated = document.getElementById("contenedorAcumulador1");
 animated.addEventListener('animationiteration', () => {
 formas += acumuladores;
 actualizarDatos();
 });
-//Hacer que el valor de progress aumente a una velocidad establecida por la variable del mismo nombre.
 
+const animated2 = document.getElementById("contenedorProgreso2");
+animated2.addEventListener('animationiteration', () => {
+acumuladores += spawners;
+actualizarDatos();
+});
 
